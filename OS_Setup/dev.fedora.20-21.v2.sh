017a0e4b2298cad8104621ff9628c2bc43b5df3d
Aaron Woods <woodyhavoc@gmail.com>
2015-02-24 10:45:19 -0700

    Fixed some typos in the script. Removed trailing slashes on the text for the google repo. Added an install option for a different login manager which should fix login freezing issues.

--- OS_Setup/dev.fedora.20-21.v2.sh
+++ OS_Setup/dev.fedora.20-21.v2.sh
@@ -4,25 +4,40 @@ USER=""
 ONYXPASS=""
 DRIVEPATH=""
 
+# Full path to the directory that you want to make i.e. /mnt/Storage
+dir=""
+
+# Accept ssh key the first time
+ssh $USER@onyx.boisestate.edu < "yes"
+
 # Pull down all the .* folders to get system settings back in place
-scp -r https://onyx.boisestate.edu:/home/students/$USER/.* /home/\u/ < $ONYXPASS &
+scp -r $USER@onyx.boisestate.edu:/home/students/$USER/.* ~/ < $ONYXPASS &
 
 # Make sure to find the correct drive letter and change /dev/sda1 to the correct drive before running
-sudo sh -ec "echo \"\u ALL=(ALL) NOPASSWD: ALL\" >> /etc/sudoers"
+sudo sh -c "echo \"$USER ALL=(ALL) NOPASSWD: ALL\" >> /etc/sudoers"
 
-sudo mkdir /mnt/$d
-sudo sh -c "echo \"$DRIVEPATH     /mnt/Storage     ntfs     defaults     0 0\" >> /etc/fstab"
+sudo mkdir $dir
+sudo sh -c "echo \"$DRIVEPATH     $dir     ntfs     defaults     0 0\" >> /etc/fstab"
 sudo mount -a
+
+# Insert path to these directories
+
+# rm -rf Documents Downloads Music Videos Pictures
+# ln -s $dir/to/Documents ~/
+# ln -s $dir/to/Downloads ~/
+# ln -s $dir/to/Music ~/
+# ln -s $dir/to/Videos ~/
+# ln -s $dir/to/Pictures ~/
 
 source ~/.bashrc
 
 # Install Google Chrome
-echo -e "[google-chrome] \
-	  \nname=google-chrome - \$basearch \
-	 \nbaseurl=http://dl.google.com/linux/chrome/rpm/stable/\$basearch \
-	  \nenabled=1 \
-	  \ngpgcheck=1 \
-	  \ngpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub" > /etc/yum.repos.d/google-chrome.repo
+echo -e "[google-chrome]
+	     \nname=google-chrome - \$basearch
+	     \nbaseurl=http://dl.google.com/linux/chrome/rpm/stable/\$basearch
+	     \nenabled=1
+	     \ngpgcheck=1
+	     \ngpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub" > /etc/yum.repos.d/google-chrome.repo
 
 sudo yum install -y kate #callback to update databases for later installs/updates
 
@@ -33,10 +48,17 @@ sudo yum localinstall -y --nogpgcheck http://download1.rpmfusion.org/free/fedora
 sudo yum groupinstall "Development Tools"
 
 #popular and development packages
-sudo yum install -y sl oogle-chrome-stable gstreamer* gstreamer-ffmpeg gstreamer* faad2 faac libdca wget compat-libstdc++-33 compat-libstdc++-296  xine-lib-extras-freeworld kdeartwork eclipse-fedora* gvim vim readline readline-devel doxygen rhythmbox libreoffice transmission subversion mono-devel git
-
+sudo yum install -y sl google-chrome-stable gstreamer* gstreamer-ffmpeg gstreamer* faad2 faac libdca wget compat-libstdc++-33 compat-libstdc++-296  xine-lib-extras-freeworld kdeartwork java-1.8.0-openjdk eclipse-fedora* gvim vim readline readline-devel doxygen rhythmbox libreoffice transmission subversion mono-devel git
 
 #OPTIONAL Fix terminal prompt, only uncomment "export" line
 
 #<username> [<working directory>] >$
-#export PS1="\u [/\W ]>$ " 
+#export PS1="\u [/\W ]>$ "
+
+# To fix the issue where the login screen freezes, use the following if needed.
+# I cannot guarantee that it will solve the problem, but I have not had any issues
+# since I did this.
+
+# sudo yum -y install lightdm system-switch-displaymanager
+# sudo system-switch-displaymanager lightdm
+# sudo reboot
